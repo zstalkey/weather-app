93d98c25e3567e6bcc176751d1e84e9533c77c69
Zoe <Zoe.Stalkey@gmail.com>
2019-11-02 12:47:00 +0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import AppContainer from './containers/AppContainer';
+import ComplaintPage from './pages/ComplaintPage';
+import WeatherPage from './pages/WeatherPage';
 
 function App() {
   return (
     <>
       <AppContainer />
+      <div>
+        <ul>
+          <li><Link to="./pages/WeatherPage">Home</Link></li>
+          <li><Link to="./pages/ComplaintPage">Complaints</Link></li>
+        </ul>
+
+        <Route path="/" exact component={WeatherPage} />
+        <Route path="/complaints" exact component={ComplaintPage} />
+      </div>
     </>
   );
 }
